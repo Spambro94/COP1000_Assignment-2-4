1100577a7c954e1252b983891f87fde8311b177e
Spambro94 <spambro94@gmail.com>
2025-03-02 18:10:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 # input statements
-salary = float(input(1250.0))
-numDependents = float(input(2))
-stateTax = float(input(0.065))
-federalTax = float(input(0.28))
-dependentsDeduction = float(input(0.025))
-totalWithheld = float(input(stateTax + federalTax + dependentsDeduction))
-takeHomePay = float(input(salary - totalWithheld))
-
-
+salary = float(input())
+numDependents = float(input())
 
 # calculate taxes here
-stateTax = salary * .065
-federalTax = salary * .28
-dependentsDeduction = numDependents * .025
-totalWithheld = stateTax + federalTax + dependentsDeduction
-takeHomePay = salary - totalWithheld
 
 # output statements
 print("Salary: $" + str(salary))
